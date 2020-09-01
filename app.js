a47380f2f2bdf246d0f73f5935e3e3da54ee1b11
Salah Ashraf <65281113+salahashraf3@users.noreply.github.com>
2020-09-01 15:53:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgslider(anything) {
+    document.querySelector("#pepsi").src = anything;
+}
+
+function ChangeBgColor(color) {
+    const bd = document.querySelector(".sec");
+    bd.style.background = color;
+}
